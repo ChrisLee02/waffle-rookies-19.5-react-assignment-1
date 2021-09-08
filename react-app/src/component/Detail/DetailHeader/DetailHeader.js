98d2d8ca9878e77134fd0bf91820fbc8f92b961c
ChrisLee02 <hdchris765@snu.ac.kr>
2021-09-08 10:37:11 +0900

    조건에 따라 수정 or 경고창 구현

--- react-app/src/component/Detail/DetailHeader/DetailHeader.js
+++ react-app/src/component/Detail/DetailHeader/DetailHeader.js
@@ -3,8 +3,14 @@ import './DetailHeader.css'
 const DetailHeader = (props) => {
     return (
         <div className={'DetailHeader'}>
-            <button onClick={()=>{
-                props.modifyStudent(props.changedData.id,props.changedData);
+            <button onClick={()=>{ //수정 조건: 한글 2, 3글자 & 학년은 1, 2, 3중 하나
+                const korean = /[가-힣]/;
+                if( (1<props.changedData.name.length<4) && korean.test(props.changedData.name[0]) && korean.test(props.changedData.name[0]) &&  (props.changedData.grade === 1 || props.changedData.grade === 2 || props.changedData.grade === 3 )  ) {
+                    props.modifyStudent(props.changedData.id,props.changedData);
+                } else{
+                    window.alert('이름은 한글 2~3글자, 학년은 1,2,3 중 하나로 수정할 수 있습니다.');
+                }
+
             }} id={'Save'}>저장</button>
             <button onClick={()=>{
                 props.setnowstuData({ // 현재 선택된 학생 데이터는 '없다'로 설정
