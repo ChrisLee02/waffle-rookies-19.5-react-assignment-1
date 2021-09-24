fe2e2413d48cf6645785619549ca3ab848a26ca5
ChrisLee02 <hdchris765@snu.ac.kr>
2021-09-24 15:23:12 +0900

    라우팅 및 AWS 관련 버그 수정

--- src/component/Detail/DetailHeader/DetailHeader.js
+++ src/component/Detail/DetailHeader/DetailHeader.js
@@ -3,7 +3,7 @@ import {useStudentContext} from '../../../context/Context';
 import image from '../../../Data/PageMove.svg'
 import {useHistory} from 'react-router-dom';
 
-const DetailHeader = (props) => {
+const DetailHeader = () => {
     const context = useStudentContext();
     const history = useHistory();
     return (
@@ -11,7 +11,7 @@ const DetailHeader = (props) => {
             <div></div>
             <button onClick={() => {
                 context.setNowStudentData(context.studentData.filter(student=>student.id===context.nowStudentData.id)[0]);
-                history.push('/students/'+context.nowStudentData.id.toString())
+                history.push('/students/'+context.nowStudentData.id.toString());
             }} id={'GoDetail'}><img alt='' src={image}/></button>
 
         </div>
